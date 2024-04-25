37c0e36fe12ca028b4800bd7edb4018bf93d3c16
wishon1 <wisdomhonest1@gmail.com>
2024-04-25 15:20:58 -0400

    add change to task 1

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,4 +1,4 @@
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
   const arr = [];
@@ -6,11 +6,11 @@ export default function initializeRooms() {
   const firstClassRoom = new ClassRoom(9);
   arr.push(firstClassRoom);
 
-  const secondClassRoom = new ClassRoom(20)
+  const secondClassRoom = new ClassRoom(20);
   arr.push(secondClassRoom);
 
   const thirdClassRoom = new ClassRoom(34);
   arr.push(thirdClassRoom);
 
   return arr;
-};
+}

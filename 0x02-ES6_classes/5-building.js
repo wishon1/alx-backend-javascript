1a9a465118ccbb0a1482de2b0eac07192b88ac21
wishon1 <wisdomhonest1@gmail.com>
2024-04-25 18:41:37 -0400

    add task 6

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,13 @@
+export default class Building {
+  constructor(sqft) {
+    if (new.target !== Building && this.evacuationWarningMessage === undefined) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+    this._sqft = sqft;
+  }
+
+  // getter
+  get sqft() {
+    return this._sqft;
+  }
+}

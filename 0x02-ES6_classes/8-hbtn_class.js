32f86d446188d04b218fc45b72e0758510f1f32c
wishon1 <wisdomhonest1@gmail.com>
2024-04-26 06:49:59 -0400

    add task 8 and 9

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,14 +1,14 @@
 export default class HolbertonClass {
-    constructor(size, location) {
-      this._size = size;
-      this._location = location;
-    }
-  
-    [Symbol.toPrimitive](hint) {
-      if (hint === 'string') {
-        return this._location;
-      }
-  
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
       return this._size;
     }
+
+    return this._location;
   }
+}

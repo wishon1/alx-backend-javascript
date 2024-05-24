6200068543dc723e706dca3b567a30d85e9f7f8c
wishon1 <wisdomhonest1@gmail.com>
2024-05-24 17:18:32 -0400

    added docstring

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,3 +1,19 @@
+/**
+  * function named guardrail that will accept one argument mathFunction (Function).
+  * This function should create and return an array named queue.
+  * When the mathFunction function is executed, the value returned by the function
+  * should be appended to the queue. If this function throws an error, the error message
+  * should be appended to the queue. In every case, the message Guardrail was processed
+  * should be added to the queue.
+  * Example:
+  *
+  * [
+  *   1000,
+  *   'Guardrail was processed',
+  * ]
+  *
+  * @ Retur: return a value else an error message
+  */
 export default function guardrail(mathFunction) {
   const queue = [];
 

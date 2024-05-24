da3278ffa2e5283f7353f8326a8b7ff006a826a1
wishon1 <wisdomhonest1@gmail.com>
2024-05-24 09:36:06 -0400

    add docstrings to task 11 and 12

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,3 +1,20 @@
+/**
+  * Write a function named createEmployeesObject that will receive two
+  * arguments:
+  * departmentName (String)
+  * employees (Array of Strings)
+  * export default function createEmployeesObject(departmentName, employees) {
+  * }
+  * The function should return an object with the following format:
+  *
+  * {
+  *   $departmentName: [
+  *        $employees,
+  *  ],
+  * }
+  *
+  * @Returns(object)
+  */
 export default function createEmployeesObject(departmentName, employees) {
   const employeesObjt = {
     [departmentName]: employees,
--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,3 +1,29 @@
+/**
+  * Write a function named createReportObject whose parameter, employeesList,
+  * is the return value of the previous function createEmployeesObject.
+  *
+  * export default function createReportObject(employeesList) {
+  *
+  * }
+  * createReportObject should return an object containing the key allEmployees
+  * and a method property called getNumberOfDepartments.
+  * allEmployees is a key that maps to an object containing the department name
+  * and a list of all the employees in that department. If you’re having trouble,
+  * use the spread syntax.
+  * The method property receives employeesList and returns the number of
+  * departments. I would suggest suggest thinking back to the ES6 method property
+  * syntax.
+  * {
+  * allEmployees: {
+  *    engineering: [
+  *         'John Doe',
+  *         'Guillaume Salva',
+  *     ],
+  *   },
+  * };
+  *
+  * @Return{dictionary}: number of departments
+  */
 export default function createReportObject(employeesList) {
   return {
     allEmployees: { ...employeesList },

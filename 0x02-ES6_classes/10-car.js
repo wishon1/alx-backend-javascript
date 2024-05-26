1d400dabdc0ac44c21bc451590cd35d6ecf8d393
wishon1 <wisdomhonest1@gmail.com>
2024-05-26 15:09:42 -0400

    add docstrings to task 9 and 10

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,3 +1,15 @@
+/**
+  * Implement a class named Car:
+  * Constructor attributes:
+  *   brand (String)
+  *   motor (String)
+  *   color (String)
+  * Each attribute must be stored in an “underscore” attribute version (ex: name is stored in _name)
+  * Add a method named cloneCar. This method should return a new object of the class.
+  * Hint: Symbols in ES6
+  *
+  * @Return: return a new object of the class.
+  */
 export default class Car {
   constructor(brand, motor, color) {
     this._motor = motor;

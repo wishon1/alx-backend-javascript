6bf65f305e4d0ea8772ebf593057d76ca332e28c
wishon1 <wisdomhonest1@gmail.com>
2024-06-15 15:44:38 -0400

    add task 0

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,13 @@
+/**
+ * create a function named displayMessage that
+ * prints in STDOUT the string argument.
+ *
+ * @param {string} msg - The mssage to be printed
+ *
+ * Return: STDOUT the string argument.
+ */
+function displayMessage(msg) {
+  console.log(msg);
+}
+
+module.exports = displayMessage;

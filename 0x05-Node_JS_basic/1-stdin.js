7b899319ed5e0f277bd1e49f306f683967c91c4f
wishon1 <wisdomhonest1@gmail.com>
2024-06-17 09:19:28 -0400

    add task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -10,9 +10,18 @@
  */
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  console.log(`Your name is: ${name}`);
-  process.stdout.write('This important software is now closing\n');
-  process.exit();
-});
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (input) => {
+    const name = input.toString();
+    process.stdout.write(`Your name is: ${name}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (input) => {
+    process.stdout.write(`Your name is: ${input.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}

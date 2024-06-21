2685af450d2e98bfb3733629d11ef41bfebf4ab1
wishon1 <wisdomhonest1@gmail.com>
2024-06-21 11:51:52 -0400

    add task 1

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,20 +1,21 @@
-const assert = require("assert");
-const calculateNumber = require("./1-calcul")
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+
 /**
- * conntain tesyt cases for this function: function calculateNumber()
+ * contain test cases for this function: calculateNumber()
  */
 
-describe("calculateNUmber", () => {
-    it("if type=='SUM' and both the other two arguements are floats", () => {
-        assert.strictEqual(("SUM", 1.4, 4.5), 6)
+describe('calculateNumber', () => {
+    it("if type=='SUM' and both the other two arguments are floats", () => {
+        assert.strictEqual(calculateNumber("SUM", 1.4, 4.5), 6);
     });
 
-    it("if type=='SUBTRACT' and both the other two arguements are floats", () => {
-        assert.strictEqual(("SUBTRACT", 1.4, 4.5), -4)
+    it("if type=='SUBTRACT' and both the other two arguments are floats", () => {
+        assert.strictEqual(calculateNumber("SUBTRACT", 1.4, 4.5), -4);
     });
 
-    it("if type=='DIVIDE' and both the other two arguements are floats", () => {
-        assert.strictEqual(("DIVIDE", 1.4, 4.5), 0.2)
-        assert.strictEqual(("DIVIDE", 1.4, 0), "Error")
+    it("if type=='DIVIDE' and both the other two arguments are floats", () => {
+        assert.strictEqual(calculateNumber("DIVIDE", 1.4, 4.5), 0.2);
+        assert.strictEqual(calculateNumber("DIVIDE", 1.4, 0), "Error");
     });
 });
